01de77b13825a58eaac0db77e2bb4cd18b146ba6
danileau <danilo.licitra@gmail.com>
2019-01-08 20:59:59 +0100

    making progress

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: danileau
+ * Date: 08.01.2019
+ * Time: 20:50
+ */
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ArticleController
+{
+    /**
+     * @Route("/")
+     */
+    public function homepage() {
+        return new Response('OMG');
+    }
+
+    /**
+     * @Route("/news/{slug}")
+     */
+    public function show($slug) {
+        return new Response(sprintf(
+        'Future page to show the article: %s',
+        $slug
+    ));
+    }
+}
